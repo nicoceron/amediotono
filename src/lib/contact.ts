c18a5a8455e2ce98f041a3375132127b9c5c6f62
nicoceron <nicocerond@gmail.com>
2026-07-12 00:45:16 -0500

    Update WhatsApp contact number

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,5 +1,5 @@
-export const WHATSAPP_NUMBER = "573228725396";
-export const WHATSAPP_DISPLAY = "+57 322 8725396";
+export const WHATSAPP_NUMBER = "573132340197";
+export const WHATSAPP_DISPLAY = "+57 313 2340197";
 export const INSTAGRAM_URL = "https://instagram.com/amediotonomusic";
 export const CONTACT_EMAIL = "amediotonomusic@gmail.com";
 
